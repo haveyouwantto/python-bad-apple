3f21439fc2dada4a78b0ce488dc82dbec1b86512
haveyouwantto <haveyouwantto@gmail.com>
2020-11-14 02:29:39 +0800

    Compress

--- badapple.py
+++ badapple.py
@@ -1,29 +1,46 @@
 # -*- coding:utf8 -*-
 
-from PIL import Image
 import time
+import gzip
 
+import threading
 
-def draw(img):
+class MP3(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.mp3='bad_apple.mp3'
+        self.setDaemon(True)
+    def run(self):
+        playsound(self.mp3)
+
+def draw(data):
     w = 0
-    mw = img.width
-    str = ""
-    for pixel in iter(img.getdata()):
-        # print(pixel)
-        if(w == mw):
+    mw = 40
+    string = ""
+    for pixel in data:
+        if(w == 40):
             w = 0
-            str += "\n"
+            string += "\n"
         w += 1
         if(pixel == 0):
-            str += "  "
+            string += "  "
         else:
-            str += "##"
-    print(str)
+            string += "##"
+    print(string+'\033[30A')
 
 
 t = time.time()
-for i in range(1, 6572):
-    im = Image.open("./bad-apple/img"+str(i)+".png")
-    draw(im)
-    while time.time() < (t+(1/30*i)):
-        time.sleep(0.001)
+i = 0
+try:
+    from playsound import playsound
+    mp3=MP3()
+    mp3.start()
+except:
+    pass
+size = 1200
+with gzip.open('bad-apple.dat.gz') as f:
+    while i < 6573:
+        i = int((time.time()-t)*30)+1
+        f.seek(i*size)
+        draw(f.read(1200))
+        time.sleep(0.02)
